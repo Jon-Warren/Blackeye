9a75df025110589f64ec396b04ade15c03a7eeca
Tyler Garcia <iamtyler3323@gmail.com>
2014-10-27 19:13:30 -0400

    Added parser functionality, added 'Add feed' button

--- Blackeye/src/org/scrumptious/blackeye/CastParser.java
+++ Blackeye/src/org/scrumptious/blackeye/CastParser.java
@@ -18,55 +18,159 @@ public class CastParser extends AsyncTask {
 	private String firstUrl;
 	private InputStream inputStream;
 	private XmlPullParser xmlParser;
-	public static final HashMap<String,HashMap<String,String>> podcasts = new HashMap();
+	public String URL;
+	public static final HashMap<String,Cast> podcasts = new HashMap();
 	public CastParser(String path) {
 		this.firstUrl = path;
+		this.URL = path;
+		
+	}
+
+	
+	public void readXML() throws Exception{
 		try {
-			URL url = new URL(path);
+		while(xmlParser.nextTag() != -1) {
+			if(xmlParser.getName().contains("owner")) {
+				xmlParser.next();
+				xmlParser.next();
+			}
+			if(xmlParser.getName().equals("image")) {
+				//Log.d("ImageTag","Found");
+				try {
+					try {
+					xmlParser.require(XmlPullParser.START_TAG, null, "image");
+					} catch(Exception e) {continue;}
+					xmlParser.nextTag();
+					xmlParser.require(XmlPullParser.START_TAG,null,"url");
+					//Log.d("ImageURL",xmlParser.nextText());
+					try {
+					xmlParser.require(XmlPullParser.END_TAG,null,"url");
+					} catch(Exception e) {
+						xmlParser.nextText();
+						//xmlParser.nextTag();
+						xmlParser.require(XmlPullParser.END_TAG,null,"url");
+					}
+					xmlParser.nextTag();
+					xmlParser.require(XmlPullParser.START_TAG,null,"title");
+					//Log.d("ImageTitle",xmlParser.nextText());
+					xmlParser.nextText();
+					xmlParser.require(XmlPullParser.END_TAG,null,"title");
+					xmlParser.nextTag();
+					xmlParser.require(XmlPullParser.START_TAG,null,"link");
+					//Log.d("ImageLink",xmlParser.nextText());
+					xmlParser.nextText();
+					xmlParser.require(XmlPullParser.END_TAG,null,"link");
+					xmlParser.nextTag();
+					xmlParser.require(XmlPullParser.END_TAG, null, "image");
+					//continue;
+				} catch(Exception e) {
+					e.printStackTrace();
+				}
+				xmlParser.nextTag();
+			}
+			if(xmlParser.getName().equals("item")) {
+				//Log.d("Cast","Cast found! Grabbing data...");
+				Cast cast = new Cast();
+				HashMap<String,String> map = new HashMap<String,String>();
+
+				int x = -1;
+				while(((x = xmlParser.nextTag()) == XmlPullParser.START_TAG) || x == XmlPullParser.END_TAG && !xmlParser.getName().equals("item")) {
+					if(xmlParser.getName().contains("organization")) {
+						//Log.d("Hrm","...");
+						xmlParser.nextTag();
+						xmlParser.nextText();
+						xmlParser.nextTag();
+						xmlParser.nextText();
+						xmlParser.nextTag();
+						//xmlParser.nextTag();
+						//Log.d("Hi",xmlParser.getName());
+						//xmlParser.nextTag();
+					}
+					try {
+						switch(xmlParser.getName()) {
+							case "title":
+								cast.setTitle(xmlParser.nextText());
+								//xmlParser.nextTag();
+								break;
+							case "description":
+								cast.setDescription(xmlParser.nextText());
+								//xmlParser.nextTag();
+								break;
+							case "pubDate":
+								cast.setPubDate(xmlParser.nextText());
+								//xmlParser.nextTag();
+								break;
+							case "link":
+								cast.setURL(xmlParser.nextText());
+								//xmlParser.nextTag();
+								break;
+							case "guid":
+								try {
+									cast.setURL(xmlParser.nextText());
+								} catch(Exception e) {
+									
+								}
+								//xmlParser.nextTag();
+								break;
+							case "itunes:summary":
+								cast.setSummary(xmlParser.nextText());
+								//xmlParser.nextTag();
+								break;
+							case "itunes:keywords":
+								cast.setKeywords(xmlParser.nextText());
+								//xmlParser.nextTag();
+								break;
+							case "itunes:duration":
+								cast.setDuration(xmlParser.nextText());
+								//xmlParser.nextTag();
+								break;
+							case "itunes:explicit":
+								xmlParser.nextText();
+								//xmlParser.nextTag();
+								break;
+							case "itunes:author":
+								cast.setDuration(xmlParser.nextText());
+								break;
+						}
+					} catch(Exception e) {e.printStackTrace();}
+				}
+				podcasts.put(cast.getTitle(), cast);
+			}
+            try {
+                try {xmlParser.nextText();} catch(Exception e) {}
+            } catch(Exception e) {
+            	e.printStackTrace();
+            }
+        }
+		} catch(Exception e) {}
+		for(String i : podcasts.keySet()) {
+			//Log.d("Podcast",i);
+		}
+	}
+
+	@Override
+	protected Object doInBackground(Object... arg0) {
+		// TODO Auto-generated method stub
+		try {
+			URL url = new URL(this.firstUrl);
 			inputStream = url.openStream();
 		} catch (Exception e) {
-			Log.e("ParserError","Failed to open stream...");
+			//Log.e("ParserError","Failed to open stream...\n");
+			e.printStackTrace();
 		}
 		try {
 			xmlParser = XmlPullParserFactory.newInstance().newPullParser();
 		} catch (XmlPullParserException e) {
-			Log.e("ParserError","Failed to instantiate PullParser");
+			//Log.e("ParserError","Failed to instantiate PullParser");
 		}
 		if(xmlParser != null && inputStream != null) {
 			try {
 				xmlParser.setInput(inputStream,null);
 				
 			} catch (Exception e) {
-				Log.e("ParserError","Failed to set parser input");
+				//Log.e("ParserError","Failed to set parser input");
 			}
 		}
-		
-	}
-
-	
-	public void readXML() throws Exception{
-		while(xmlParser.nextTag() == XmlPullParser.START_TAG) {
-			if(xmlParser.getName().contains("<item>")) {
-				Cast cast = new Cast();
-				HashMap<String,String> map = new HashMap<String,String>();
-				while(xmlParser.nextTag() != -1 && !xmlParser.getName().contains("/item")) {
-					if(xmlParser.getName().contains("title")) {
-						Log.d("TitleDebug","Adding title: "+xmlParser.getName());
-						cast.setTitle(xmlParser.getName());
-						map.put("title",cast.getTitle());
-					}
-				}
-				podcasts.put(cast.getTitle(), map);
-			}
-            try {
-                Log.d("ParserDebug", xmlParser.getName() + " | " + xmlParser.nextText());
-            } catch(Exception e) {}
-        }
-	}
-
-	@Override
-	protected Object doInBackground(Object... arg0) {
-		// TODO Auto-generated method stub
 		try {
 			readXML();
 		} catch (Exception e) {
@@ -75,4 +179,5 @@ public class CastParser extends AsyncTask {
 		}
 		return null;
 	}
+
 }
--- Blackeye/src/org/scrumptious/blackeye/MainActivity.java
+++ Blackeye/src/org/scrumptious/blackeye/MainActivity.java
@@ -1,12 +1,17 @@
 package org.scrumptious.blackeye;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -21,15 +26,80 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
 
 
 public class MainActivity extends Activity {
-    public static final ArrayList<MyParser> parsers = new ArrayList<MyParser>();
+    public static final ArrayList<CastParser> parsers = new ArrayList<CastParser>();
+    private static final ArrayList<String> feedURLS = new ArrayList<String>();
+    LinearLayout linearLayout = new LinearLayout(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        new CastParser("http://www.sciencefriday.com/audio/scifriaudio.xml").execute();
+        //setContentView(R.layout.activity_main);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        CastParser x = new CastParser("http://www.sciencefriday.com/audio/scifriaudio.xml");
+        //Log.d("Hihi","Next");
+        //CastParser y = new CastParser("http://www.npr.org/rss/podcast.php?id=510298");
+        
+        final Button btn = new Button(this);
+        btn.setText("Add Feed");
+        btn.setOnTouchListener(new View.OnTouchListener() {
+			@SuppressWarnings("deprecation")
+			@Override
+			public boolean onTouch(View arg0, MotionEvent arg1) {
+				// TODO Auto-generated method stub
+				AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this); //Read Update
+                alertDialog.setTitle("Add Feed");
+                alertDialog.setMessage("Enter feed URL");
+                final EditText box = new EditText(MainActivity.this);
+                alertDialog.setView(box);
+                
+                alertDialog.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                   public void onClick(DialogInterface dialog, int which) {
+                      // here you can add functions
+                	   feedURLS.add(box.getText().toString());
+                	   CastParser parser = new CastParser(box.getText().toString());
+                	   parser.execute();
+                	   try {
+						parser.get();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (ExecutionException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+                	   
+                	   parsers.add(parser);
+                	   linearLayout = new LinearLayout(MainActivity.this);
+                	   linearLayout.addView(btn);
+                	   for(String parserKey : CastParser.podcasts.keySet()) {
+                       	
+                       	Cast c = CastParser.podcasts.get(parserKey);
+                       	Log.d("Cast",c.getTitle());
+                       	Button button = new Button(MainActivity.this);
+                       	TextView text = new TextView(MainActivity.this);
+                       	LinearLayout layout = new LinearLayout(MainActivity.this);
+                       	layout.setOrientation(LinearLayout.HORIZONTAL);
+                       	text.setText(c.getTitle());
+                       	button.setText("Watch");
+                       	layout.addView(text);
+                       	layout.addView(button);
+                       	linearLayout.addView(layout);
+                       }
+                	   setContentView(linearLayout);
+                   }
+                });
+                
+                alertDialog.create().show();	
+				return false;
+			}
+
+        });
+        linearLayout.addView(btn);
+        
+        setContentView(linearLayout);
     }
 
 
